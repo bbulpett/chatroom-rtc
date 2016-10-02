5486e03ea8f1cdba77346f6f094ff3a0bcdeae5c
Barnabas Bulpett <bbulpett@charter.net>
2016-10-01 22:20:47 -0400

    Initial static layout

--- config/dev.exs
+++ config/dev.exs
@@ -37,7 +37,7 @@ config :phoenix, :stacktrace_depth, 20
 config :chatroom, Chatroom.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "4kayamoses",
   database: "chatroom_dev",
   hostname: "localhost",
   pool_size: 10
